a4255ad698a58b9a6795a166b266a951be3c441f
Jason Brochu <jbrochu1@gmail.com>
2022-10-19 21:30:41 -0600

    finished

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, onDeleteToy, onUpdateToy}) {
+  
+  const toyCards = toys.map((toy) =>
+  <ToyCard
+    key={toy.id}
+    id={toy.id}
+    // name={toy.name}
+    // image={toy.image}
+    // likes={toy.likes}
+    toy={toy}
+    onDeleteToy={onDeleteToy}
+    onUpdateToy={onUpdateToy}
+  />
+  )
+  
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyCards}</div>
   );
 }
 
